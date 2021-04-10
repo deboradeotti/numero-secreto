92a3ab5af7ec45d6e4219917853c253414995c36
Débora Deotti <81835864+deboradeotti@users.noreply.github.com>
2021-04-10 20:09:43 -0300

    Update secretoscript.js

--- secretoscript.js
+++ secretoscript.js
@@ -7,25 +7,25 @@ function enviar() {
   var numero = entrada.value
 
   if (numero == numeroSorteado){
-    var resultado = document.querySelector(".resultado").innerHTML = "O número secreto você acertou. Que a Força esteja com você."
+    var resultado = document.querySelector(".resultado").innerHTML = "O número secreto você acertou.<br />Que a Força esteja com você."
     document.getElementById('entrada').disabled = true
     document.getElementById('envia').disabled = true
     document.querySelector(".final").innerHTML = "<button type='submit' class='refresh' onClick='refreshPage()''>Jogar outra vez</button>"
    } 
   
   else if (numero > numeroSorteado) {
-     var resultado = document.querySelector(".resultado").innerHTML = "Menor o número secreto é. Tentar novamente você deve."
+     var resultado = document.querySelector(".resultado").innerHTML = "Menor o número secreto é.<br />Tentar novamente você deve."
      tentativas = tentativas - 1
    } 
   
   else if (numero < numeroSorteado) {
-     var resultado = document.querySelector(".resultado").innerHTML = "Maior o número secreto é. Tentar novamente você deve."
+     var resultado = document.querySelector(".resultado").innerHTML = "Maior o número secreto é.<br />Tentar novamente você deve."
      tentativas = tentativas - 1
    }
 
   if (tentativas < 0) { 
 
-    var resultado = document.querySelector(".resultado").innerHTML = "Mais nenhuma tentativa você tem. " + numeroSorteado + " o número secreto era.<br />Muito a aprender você ainda tem."
+    var resultado = document.querySelector(".resultado").innerHTML = "Mais nenhuma tentativa você tem.<br />" + numeroSorteado + " o número secreto era.<br />Muito a aprender você ainda tem."
     document.getElementById('entrada').disabled = true
     document.getElementById('envia').disabled = true
     document.querySelector(".final").innerHTML = "<button type='submit' class='refresh' onClick='refreshPage()''>Jogar outra vez</button>"
